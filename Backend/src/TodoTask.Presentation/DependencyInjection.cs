735f09e9f1d292c58a95e8465eedd3fe6b377e20
Alexandr <abagnenko@garage-group.io>
2025-09-10 15:00:08 +0300

    create projects, init clean architecture

---
+++ Backend/src/TodoTask.Presentation/DependencyInjection.cs
@@ -0,0 +1,38 @@
+﻿namespace TodoTask.Presentation;
+
+/// <summary>
+/// Конфигурация зависимостей приложения
+/// </summary>
+public static class DependencyInjection
+{
+    
+    /// <summary>
+    /// Конфигурация сервисов
+    /// </summary>
+    /// <param name="services">Коллекция сервисов</param>
+    public static IServiceCollection ConfigureServices(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Конфигурация конвеера приложения
+    /// </summary>
+    /// <param name="app">Веб-приложение</param>
+    public static void ConfigurePipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
+}
