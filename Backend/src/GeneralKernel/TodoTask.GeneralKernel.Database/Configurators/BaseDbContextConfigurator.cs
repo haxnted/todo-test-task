08bdf9319fa9f4d9b6d0431296964c4b3fff777d
Gargantua <bagnencko.sasha1@gmail.com>
2025-09-13 01:12:00 +0300

    create shared kernel

---
+++ Backend/src/GeneralKernel/TodoTask.GeneralKernel.Database/Configurators/BaseDbContextConfigurator.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using TodoTask.GeneralKernel.Database.Configurations;
+
+namespace TodoTask.GeneralKernel.Database.Configurators
+{
+    /// <summary>
+    /// Базовый класс конфигуратора DbContext.
+    /// </summary>
+    public abstract class BaseDbContextConfigurator<TDbContext>(
+        IConfiguration configuration,
+        ILoggerFactory loggerFactory)
+        : IDbContextOptionsConfigurator<TDbContext>
+        where TDbContext : DbContext
+    {
+        /// <summary>
+        /// Имя строки подключения.
+        /// </summary>
+        protected abstract string ConnectionStringName { get; }
+
+        /// <inheritdoc/>
+        public void Configure(DbContextOptionsBuilder<TDbContext> options)
+        {
+            var connectionString = configuration.GetConnectionString(ConnectionStringName)
+                ?? throw new InvalidOperationException($"Строка подключения '{ConnectionStringName}' не найдена.");
+
+            options
+                .UseLoggerFactory(loggerFactory)
+                .UseNpgsql(connectionString, npgsqlOptions =>
+                {
+                    npgsqlOptions.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+                    npgsqlOptions.CommandTimeout(60);
+                    npgsqlOptions.EnableRetryOnFailure();
+                })
+                .EnableSensitiveDataLogging(); 
+        }
+    }
+}
