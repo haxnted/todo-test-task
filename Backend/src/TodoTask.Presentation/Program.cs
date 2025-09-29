7dd0ae33358ad3b236abaeb70afc9c264337f80a
Gargantua <bagnencko.sasha1@gmail.com>
2025-09-29 04:08:35 +0300

    feat: add seeder issues

--- Backend/src/TodoTask.Presentation/Program.cs
+++ Backend/src/TodoTask.Presentation/Program.cs
@@ -1,3 +1,4 @@
+using TodoTask.GeneralKernel.Database.Abstracts;
 using TodoTask.Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.ConfigureServices(builder.Configuration);
 
 var app = builder.Build();
+
+// // Заполнение базы данных тестовыми данными
+// using (var scope = app.Services.CreateScope())
+// {
+//     var seeders = scope.ServiceProvider.GetServices<ISeeder>();
+//
+//     foreach (var seeder in seeders)
+//     {
+//         await seeder.SeedAsync(CancellationToken.None);
+//     }
+// }
 
 app.ConfigurePipeline();
 app.Run();
